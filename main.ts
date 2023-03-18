5ffe872102d32e2834cd8ec52d17fb5e45a4d4e9
eugene1g <eugene7@gmail.com>
2023-03-18 10:03:20 -0400

    test plugins

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-import ModernError from 'modern-errors'
+import ModernError from "modern-errors";
+import modernErrorsClean from "modern-errors-clean";
 
-export const BaseError = ModernError.subclass('BaseError')
+export const BaseError = ModernError.subclass("BaseError", {
+  plugins: [modernErrorsClean],
+});
